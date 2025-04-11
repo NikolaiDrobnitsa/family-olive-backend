b94cf49fafb2b9ca467c0a60e8eb0c712af46a48
NIkolya <nikolaidrobnitsa@gmail.com>
2025-04-12 00:52:29 +0300

    add new admin block

--- routes/web.php
+++ routes/web.php
@@ -23,29 +23,29 @@ use App\Http\Controllers\Admin\SettingController;
 //Route::get('/auth/check', [CustomAuthController::class, 'checkAuth'])->name('auth.check');
 
 // Маршруты административной панели
-Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function () {
-    // Дашборд
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
-    // Управление пользователями
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
-    Route::get('/users/{id}/survey-responses', [UserController::class, 'getSurveyResponses'])->name('users.survey-responses');
-
-    // Управление событиями
-    Route::resource('events', EventController::class);
-    Route::post('/events/order', [EventController::class, 'updateOrder'])->name('events.update-order');
-
-    // Управление опросником
-    Route::resource('survey', SurveyQuestionController::class);
-    Route::post('/survey/order', [SurveyQuestionController::class, 'updateOrder'])->name('survey.update-order');
-
-    // Настройки
-    Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
-    Route::post('/settings', [SettingController::class, 'update'])->name('settings.update');
-    Route::post('/settings/password', [SettingController::class, 'changePassword'])->name('settings.change-password');
-    Route::post('/settings/admin', [SettingController::class, 'createAdmin'])->name('settings.create-admin');
-    Route::delete('/settings/admin/{id}', [SettingController::class, 'deleteAdmin'])->name('settings.delete-admin');
-});
+//Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function () {
+//    // Дашборд
+//    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+//
+//    // Управление пользователями
+//    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+//    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+//    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+//    Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
+//    Route::get('/users/{id}/survey-responses', [UserController::class, 'getSurveyResponses'])->name('users.survey-responses');
+//
+//    // Управление событиями
+//    Route::resource('events', EventController::class);
+//    Route::post('/events/order', [EventController::class, 'updateOrder'])->name('events.update-order');
+//
+//    // Управление опросником
+//    Route::resource('survey', SurveyQuestionController::class);
+//    Route::post('/survey/order', [SurveyQuestionController::class, 'updateOrder'])->name('survey.update-order');
+//
+//    // Настройки
+//    Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
+//    Route::post('/settings', [SettingController::class, 'update'])->name('settings.update');
+//    Route::post('/settings/password', [SettingController::class, 'changePassword'])->name('settings.change-password');
+//    Route::post('/settings/admin', [SettingController::class, 'createAdmin'])->name('settings.create-admin');
+//    Route::delete('/settings/admin/{id}', [SettingController::class, 'deleteAdmin'])->name('settings.delete-admin');
+//});
